09a6e9a0b46715fccdc53445d6823e4926d13cda
waris <m.waismahmood@gmail.com>
2026-02-24 23:46:53 +0500

    Update about

--- components/about.tsx
+++ components/about.tsx
@@ -28,7 +28,7 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3 text-lg text-justify">
         I am from Pakistan. I am result-oriented developer with a proven track record in crafting dynamic and responsive websites. Proficient in
-        leveraging a diverse skill set including HTML, CSS, JavaScript, React, Node.js, and other cutting-edge technologies.
+        leveraging a diverse skill set including HTML, CSS, JavaScript, Next.js, React.js, Node.js, and other cutting-edge technologies.
         Demonstrated expertise in designing and implementing innovative solutions that enhance user experiences.
         I started my journey in web development out of curiosity, and it has now become my passion. 
         I aim to create dynamic, user-friendly websites that go above and beyond expectations.
